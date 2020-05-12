b2f6bb8b0f7390d7dc8dc77a384dce2b0e6d5c5a
Anthony Yoskovich <yoskovia@mail.gvsu.edu>
2020-05-12 14:19:05 -0400

    Initial commit

---
+++ src/price_check.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Claims made about poultry, pork, and beef.
+
+# ### “poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935”
+
+# In[ ]:
+
+
+
+
+# ### “Pork, now also raised mostly at factory scale indoors, is 12% cheaper”
+
+# In[ ]:
+
+
+
+
+# ### “Beef, which isn’t, costs 63% more. “
+
+# In[ ]:
+
+
+
