1c50ae8693d30e48828af24a8c3616062b306e04
Anthony Yoskovich <yoskovia@mail.gvsu.edu>
2020-05-13 10:58:56 -0400

    Rearranging text

--- price_check.md
+++ price_check.md
@@ -1,20 +1,20 @@
-# Claims made about poultry, pork, and beef.
-
-Article [here](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P).
-
 ### “poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935”
 
 ### “Pork, now also raised mostly at factory scale indoors, is 12% cheaper”
 
 ### “Beef, which isn’t, costs 63% more. “
 
-I'm going to go to FRED and get the price of Chicken, Fresh, Whole, Per Lb. (453.6 Gm) in U.S. City Average. [Here](https://fred.stlouisfed.org/series/APU0000706111#0) This way I can download the data in a nice clean format. 
 
-Series ID APU0000706111
+### Data sources
 
-I'll need to adjust for inflation, so I'll use the CPI: [CPIAUCSL](https://fred.stlouisfed.org/series/CPIAUCSL).
+In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled data from the FRED database, which pulls from the BLS. Each link will take you to the exact series that I downloaded here.
 
-**Also, I can't find where he got data going all the way back to the 30s??**
+- [CPI (for inflation)](https://fred.stlouisfed.org/series/CPIAUCSL)
+- [Poultry](https://fred.stlouisfed.org/series/APU0000706111)
+- [Pork](https://fred.stlouisfed.org/series/APU0000FD3101)
+- [Beef](https://fred.stlouisfed.org/series/APU0000703112)
+
+Not sure where he's getting data 
 
 
 ![png](price_check_files/price_check_2_0.png)
--- src/price_check.py
+++ src/price_check.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Claims made about poultry, pork, and beef.
-# 
-# Article [here](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P).
-
 # ### “poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935”
 # 
 # ### “Pork, now also raised mostly at factory scale indoors, is 12% cheaper”
 # 
 # ### “Beef, which isn’t, costs 63% more. “
 # 
-# I'm going to go to FRED and get the price of Chicken, Fresh, Whole, Per Lb. (453.6 Gm) in U.S. City Average. [Here](https://fred.stlouisfed.org/series/APU0000706111#0) This way I can download the data in a nice clean format. 
-# 
-# Series ID APU0000706111
-# 
-# I'll need to adjust for inflation, so I'll use the CPI: [CPIAUCSL](https://fred.stlouisfed.org/series/CPIAUCSL).
-# 
-# **Also, I can't find where he got data going all the way back to the 30s??**
 
-# In[2]:
+# ### Data sources
+# 
+# In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled data from the FRED database, which pulls from the BLS. Each link will take you to the exact series that I downloaded here.
+# 
+# - [CPI (for inflation)](https://fred.stlouisfed.org/series/CPIAUCSL)
+# - [Poultry](https://fred.stlouisfed.org/series/APU0000706111)
+# - [Pork](https://fred.stlouisfed.org/series/APU0000FD3101)
+# - [Beef](https://fred.stlouisfed.org/series/APU0000703112)
+# 
+# Not sure where he's getting data 
+
+# In[15]:
 
 
 import pandas as pd
@@ -54,8 +54,10 @@ df['new_pork'] = apply_adjust(df, 'pork', MOST_RECENT)
 df['new_chicken'] = apply_adjust(df, 'chicken', MOST_RECENT)
 df['new_beef'] = apply_adjust(df, 'beef', MOST_RECENT)
 
-plt.plot(df['date'], df['new_chicken'], label='chicken');
-plt.plot(df['date'], df['new_pork'], label='pork');
-plt.plot(df['date'], df['new_beef'], label='beef')
+plt.plot(df['date'], df['new_chicken'], label='Chicken');
+plt.plot(df['date'], df['new_pork'], label='Pork');
+plt.plot(df['date'], df['new_beef'], label='Beef')
 plt.legend(loc="upper left");
+plt.xlabel("Year");
+plt.ylabel("Price ($)")
 plt.title("Price adjusted for inflation");
