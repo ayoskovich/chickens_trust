67895d49e4408da0683237c5325770d7486a7337
Anthony Yoskovich <yoskovia@mail.gvsu.edu>
2020-05-14 18:05:49 -0400

    Rearranging paragraphs

--- price_check.md
+++ price_check.md
@@ -1,25 +1,19 @@
 ### Data sources
 
-In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled data from the FRED database, which pulls from the BLS. Each link will take you to the exact series that I downloaded here.
+In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled price data from FRED (which sources data from the BLS) and adjusted it using the CPI. Data sources are here:
 
 - [CPI (for inflation)](https://fred.stlouisfed.org/series/CPIAUCSL)
 - [Poultry](https://fred.stlouisfed.org/series/APU0000706111)
 - [Pork](https://fred.stlouisfed.org/series/APU0000FD3101)
 - [Beef](https://fred.stlouisfed.org/series/APU0000703112)
 
-The data from the FRED does NOT go back to 1935.
+**A note here**: I couldn't for the life of me, find any data that goes back to 1935, like Fox cites in the article. The earliest history I could find from FRED or the BLS only goes back to the 80s, so I must be missing something!
 
 
 ![png](price_check_files/price_check_1_0.png)
 
 
-Quotes taken directly from the article
-
-> 1. "poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935"
-
-> 2. "Pork, now also raised mostly at factory scale indoors, is 12% cheaper"
-
-> 3. "Beef, which isn’t, costs 63% more."
+Now I'll simply take the percentage difference between the oldest and most recent price.
 
     chicken
     	Oldest: 1980-01-01 00:00:00
@@ -37,6 +31,17 @@ Quotes taken directly from the article
     	$3.23 --> $4.05
     	pct change: 0.253
 
+
+Quotes taken directly from the article
+
+> 1. "poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935"
+    - From 1980 - 2020, chicken prices have declined 31.5%.
+
+> 2. "Pork, now also raised mostly at factory scale indoors, is 12% cheaper"
+    - From 1998 - 2020, the price of pork has declined 26%.
+
+> 3. "Beef, which isn’t, costs 63% more."
+    - From 1984 - 2020, the price of beef has increased 25.3%.
 
 ### Limitations
 
--- src/price_check.py
+++ src/price_check.py
@@ -3,14 +3,14 @@
 
 # ### Data sources
 # 
-# In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled data from the FRED database, which pulls from the BLS. Each link will take you to the exact series that I downloaded here.
+# In order to validate the claims from [this](https://www.bloomberg.com/news/articles/2020-05-11/why-chicken-is-plentiful-during-the-pandemic-and-beef-is-not?srnd=premium&utm_medium=social&utm_source=twitter&utm_campaign=socialflow-organic&utm_content=markets&cmpid%3D=socialflow-twitter-markets&sref=XQtHDW1P) article, I pulled price data from FRED (which sources data from the BLS) and adjusted it using the CPI. Data sources are here:
 # 
 # - [CPI (for inflation)](https://fred.stlouisfed.org/series/CPIAUCSL)
 # - [Poultry](https://fred.stlouisfed.org/series/APU0000706111)
 # - [Pork](https://fred.stlouisfed.org/series/APU0000FD3101)
 # - [Beef](https://fred.stlouisfed.org/series/APU0000703112)
 # 
-# The data from the FRED does NOT go back to 1935.
+# **A note here**: I couldn't for the life of me, find any data that goes back to 1935, like Fox cites in the article. The earliest history I could find from FRED or the BLS only goes back to the 80s, so I must be missing something!
 
 # In[1]:
 
@@ -61,13 +61,7 @@ plt.xlabel("Year");
 plt.ylabel("Price ($)")
 plt.title("Historic Prices of Different Meats");
 
-# Quotes taken directly from the article
-# 
-# > 1. "poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935"
-# 
-# > 2. "Pork, now also raised mostly at factory scale indoors, is 12% cheaper"
-# 
-# > 3. "Beef, which isn’t, costs 63% more."
+# Now I'll simply take the percentage difference between the oldest and most recent price.
 
 # In[2]:
 
@@ -75,6 +69,17 @@ plt.title("Historic Prices of Different Meats");
 get_diff(df, 'chicken')
 get_diff(df, 'pork')
 get_diff(df, 'beef')
+
+# Quotes taken directly from the article
+# 
+# > 1. "poultry costs U.S. consumers 62% less in inflation-adjusted terms than it did in 1935"
+#     - From 1980 - 2020, chicken prices have declined 31.5%.
+# 
+# > 2. "Pork, now also raised mostly at factory scale indoors, is 12% cheaper"
+#     - From 1998 - 2020, the price of pork has declined 26%.
+# 
+# > 3. "Beef, which isn’t, costs 63% more."
+#     - From 1984 - 2020, the price of beef has increased 25.3%.
 
 # ### Limitations
 # 
