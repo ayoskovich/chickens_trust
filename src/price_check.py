1600c7bbca0406c01b2497c7a2de390af1602733
Anthony Yoskovich <yoskovia@mail.gvsu.edu>
2020-05-25 13:32:39 -0400

    Adding new extrapolation

--- src/price_check.py
+++ src/price_check.py
@@ -11,7 +11,7 @@
 
 # The chart below shows how the average price of 3 different types of meats has changed over time.
 
-# In[1]:
+# In[5]:
 
 
 import pandas as pd
@@ -25,9 +25,9 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # https://github.com/jupyter/notebook/issues/3670
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
-get_ipython().run_line_magic('run', './helpers.ipynb')
+%matplotlib inline
+%config InlineBackend.figure_format = 'retina'
+%run ./helpers.ipynb
 
 cpi = get_cpi()
 pcs = get_prices()
@@ -35,7 +35,6 @@ pcs = get_prices()
 sns.lineplot(x='date', y='price', hue='Meat', data=pcs);
 plt.title('Meat Prices Over Time (Nominal)')
 plt.ylabel('Price');
-
 
 # To adjust for the change in purchasing power I'll use the CPI.
 
@@ -57,7 +56,6 @@ adjust.pipe(lambda x:
 plt.ylabel('Price')
 plt.title('Meat Prices Over Time (Real)');
 
-
 # In[3]:
 
 
@@ -66,16 +64,26 @@ his = {
     'Chicken':-.62,
     'Pork':-.12
 }
-x = (
-    adjust.groupby('Meat').apply(lambda x: get_diff(x, 'cur_price', 'date'))
-    .reset_index()
-    .drop(labels='level_1', axis=1)
-    .assign(NumYears = lambda x: x['End'].dt.year - x['Start'].dt.year)
-    .assign(av = lambda x: x.Change / x.NumYears)
-    .assign(Extrap = lambda x: x.Change / x.NumYears * 85)
-    .assign(his = lambda x: x['Meat'].map(his))
+
+adjust['diff'] = adjust.groupby('Meat')['cur_price'].transform(pd.Series.diff)
+adjust['avg'] = adjust.groupby('Meat')['diff'].transform(np.mean)
+adjust['keep_up'] = (
+    adjust
+    .groupby('Meat')['date']
+    .transform(lambda x: 
+               (x.min() - pd.to_datetime(date(1935, 1, 1))) / np.timedelta64(1, 'M')
+              )
 )
 
+adjust = (
+    adjust
+    .groupby('Meat')
+    .apply(lambda x: x.loc[x.date == x.date.min()])
+    .assign(Extrap = lambda x: x.cur_price + x.avg*x.keep_up*-1)
+    .rename(columns={'Meat':'meat'})
+)
+adjust['his'] = adjust['meat'].map(his)
+adjust['Extrap'] = (adjust['cur_price'] - adjust['Extrap']) / adjust['Extrap']
 
 # A couple things to note about the real prices:
 # 
@@ -96,14 +104,14 @@ bar_width = 0.35
 opacity = 0.8
 
 rects1 = plt.bar(index, 
-                 x['Extrap'], 
+                 adjust['Extrap'], 
                  bar_width,
                  alpha=opacity,
                  color='b',
                  label='Mine')
 
 rects2 = plt.bar(index + bar_width,
-                 x['his'], 
+                 adjust['his'], 
                  bar_width,
                  alpha=opacity,
                  color='g',
@@ -117,6 +125,5 @@ plt.legend()
 
 plt.tight_layout()
 plt.show()
-
 
 # I finally arrive at the above numbers. My estimates for beef and chicken are both very similar.
